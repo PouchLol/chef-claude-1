a3b1d4344a18d39cbc2ed187791a15be55811602
Karthik <karthikh2004@gmail.com>
2025-01-18 10:39:23 +0530

    conditional rendering components added

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -13,25 +13,45 @@ export default function MainContent(){
 
     let [ulElements,setUlElements]=useState([listEl])
     console.log(ulElements)
-
-
+    console.log(listEl)
     function formSubmitted(formData){
         const newIngredient = formData.get("ingredient")
         setUlElements(
             (ulElements)=>{return [...ulElements,<li>{newIngredient}</li>]}
         )
     }
-
+    
     return (
         <>
         <form className="body-form" id="form-el" action={formSubmitted}>
             <input aria-label="add-ingredient" name="ingredient" className="body-input" id="input-text" placeholder="eg: oregano"></input>
             <button className="body-button"> + Add ingredient!</button>
         </form>
-        <h1 className="ingredients-header">Ingredients in Hand: </h1>
-        <ul className="ingredients-list">
-            {ulElements}
-        </ul>
+        {ulElements.length-1 ? 
+        <section>
+            <h1 className="ingredients-header">List of ingredients:</h1>
+            <ul className="ingredients-list">
+                {ulElements}
+            </ul>
+            
+        </section>
+        : null}
+        {
+            (ulElements.length-1)>=4 &&
+            <div className="ingredients-submit">
+                <div className="ingredients-text">
+                    <h3>Ready for a recipe?</h3>
+                    <p>Generate a recipe from your list of ingredients</p>
+                </div>
+                <button className="ingredients-submit-btn">Generate</button>
+            </div>
+        }
+        <div className="recipe">
+            
+        </div>    
+        
+
+        
         </>
     )
 }
